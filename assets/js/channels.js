bc334272672d637833ceecbb5916b0ac4e4ca2d2
Rachane16 <89630315+Rachane16@users.noreply.github.com>
2026-06-22 13:12:51 +0700

    feat: add channel catalogue

---
+++ assets/js/channels.js
@@ -0,0 +1,125 @@
+/* BK16 Live channel catalogue.
+   Keep stream URLs and logos here so UI/player logic stays separate. */
+window.BK16_CHANNELS = [
+
+  {
+    id:"wc-monomax1",
+    name:"FIFA World Cup MonoMax1",
+    logo:"https://img2.pic.in.th/WorldCup.png",
+    url:"https://yyi7tewa2x.3ppnkog.com/xstream/monomax-2/playlist.m3u8"
+  },
+  {
+    id:"wc-monomax2",
+    name:"FIFA World Cup MonoMax2",
+    logo:"https://img2.pic.in.th/WorldCup.png",
+    url:"https://yyi7tewa2x.3ppnkog.com/xstream/monomax-3/playlist.m3u8"
+  },
+  {
+    id:"wc-monomax3",
+    name:"FIFA World Cup MonoMax3",
+    logo:"https://img2.pic.in.th/WorldCup.png",
+    url:"https://yyi7tewa2x.3ppnkog.com/xstream/monomax-1/playlist.m3u8"
+  },
+  {
+    id:"fifa-plus",
+    name:"FIFA+",
+    logo:"https://img2.pic.in.th/Fifa.png",
+    url:"https://a62dad94.wurl.com/master/f36d25e7e52f1ba8d7e56eb859c636563214f541/UmFrdXRlblRWLWV1X0ZJRkFQbHVzRW5nbGlzaF9ITFM/playlist.m3u8"
+  },
+  {
+    id:"fifa-plus-usa",
+    name:"FIFA+ USA",
+    logo:"https://img2.pic.in.th/Fifa.png",
+    url:"https://d2w9q46ikgrcwx.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-of5cbk3sav3w5/v1/sysdata_s_p_a_fifa_7/samsungheadend_us/latest/main/hls/playlist.m3u8"
+  },
+  {
+    id:"monomax01",
+    name:"Monomax Sport 01",
+    logo:"https://img1.pic.in.th/images/011c7ba4fae8fe556a4.png",
+    url:"https://blive99.github.io/m3u8/sporttv/monomax1.m3u8"
+  },
+  {
+    id:"monomax02",
+    name:"Monomax Sport 02",
+    logo:"https://img2.pic.in.th/maxc2d18f180c225ce1.png",
+    url:"https://blive99.github.io/m3u8/sporttv/monomax2.m3u8"
+  },
+  {
+    id:"monomax03",
+    name:"Monomax Sport 03",
+    logo:"https://img2.pic.in.th/maxc2d18f180c225ce1.png",
+    url:"https://blive99.github.io/m3u8/sporttv/monomax3.m3u8"
+  },
+  {
+    id:"redbulltv",
+    name:"Red Bull TV",
+    logo:"https://img2.sport-tv-guide.live/images/tv-station-red-bull-tv-2760.png",
+    url:"https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master_3360.m3u8"
+  },
+  {
+    id:"spotv1",
+    name:"SPOTV 1",
+    logo:"https://ball67.com/assets/channels-logo/spotv1.png",
+    url:"https://blive99.github.io/m3u8/sporttv/truespotv2.m3u8"
+  },
+  {
+    id:"spotv2",
+    name:"SPOTV 2",
+    logo:"https://ball67.com/assets/channels-logo/spotv2.png",
+    url:"https://blive99.github.io/m3u8/sporttv/truespotv.m3u8"
+  },
+  {
+    id:"bein1",
+    name:"beIN SPORT TH 1",
+    logo:"https://ball67.com/assets/channels-logo/bein1.png",
+    url:"http://54.37.19.157/bein1/tracks-v1a1/mono.m3u8"
+  },
+  {
+    id:"bein2",
+    name:"AIS beIN SPORT 2",
+    logo:"https://ball67.com/assets/channels-logo/bein2.png",
+    url:"http://54.37.19.157/bein2/tracks-v1a1/mono.m3u8"
+  },
+  {
+    id:"bein3",
+    name:"beIN SPORT TH 3",
+    logo:"https://ball67.com/assets/channels-logo/bein3.png",
+    url:"http://54.37.19.157/bein3/tracks-v1a1/mono.m3u8"
+  },
+  {
+    id:"truesport1",
+    name:"True Sport 1",
+    logo:"https://img2.pic.in.th/true1.png",
+    url:"https://www.livedoomovies.com:4431/02_sport1_720p/playlist.m3u8"
+  },
+  {
+    id:"truesport2",
+    name:"True Sport 2",
+    logo:"https://img1.pic.in.th/images/true2.png",
+    url:"https://www.livedoomovies.com:4431/02_sport1_720p/playlist.m3u8"
+  },
+  {
+    id:"truesport3",
+    name:"True Sport 3",
+    logo:"https://img1.pic.in.th/images/true3.png",
+    url:"https://www.livedoomovies.com:4431/02_sport1_720p/playlist.m3u8"
+  },
+  {
+    id:"lfctv",
+    name:"LFC TV",
+    logo:"https://img2.sport-tv-guide.live/images/tv-station-lfc-tv-112.png",
+    url:"https://dlhd.pk/watch/stream-826.php"
+  },
+  {
+    id:"fox-sport-1hd",
+    name:"Fox Sport 1HD",
+    logo:"https://img1.pic.in.th/images/F1.png",
+    url:"http://185.246.209.113/FoxSports1HD/index.m3u8"
+  },
+  {
+    id:"monomax01-backup",
+    name:"Monomax Sport 01",
+    logo:"https://img1.pic.in.th/images/011c7ba4fae8fe556a4.png",
+    url:"https://raw.githubusercontent.com/tummeng21/m3u8/refs/heads/main/max1.m3u8"
+  }
+];
